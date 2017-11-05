f5869b5ce557b8d8b3aae6478ddcdb478ed3a307
mcleppala <minna.leppala@outlook.com>
2017-11-05 14:32:17 +0200

    Update asennus.sh

--- asennus.sh
+++ asennus.sh
@@ -1,21 +1,21 @@
-echo "***************************"
+echo "*******************************************"
 echo "Asetetaan näppäimistön kieli ja aikavyöhyke"
-echo "***************************"
+echo "*******************************************"
 setxkbmap fi
 sudo timedatectl set-timezone Europe/Helsinki
-echo "***************************"
+echo "*******************************************"
 echo "Asennetaan Puppet, Tree ja Github"
-echo "***************************"
+echo "*******************************************"
 sudo apt-get update
 sudo apt-get install -y puppet tree git
-echo "***************************"
+echo "*******************************************"
 echo "Kloonataan repository ja kopioidaan tiedostot"
-echo "***************************"
+echo "*******************************************"
 git clone https://github.com/mcleppala/puppet
 sudo cp -r /home/xubuntu/puppet/modules/apache2 /etc/puppet/modules/
 sudo cp -r /home/xubuntu/puppet/modules/sshd /etc/puppet/modules/
 sudo cp -r /home/xubuntu/puppet/modules/mysql /etc/puppet/modules/
 sudo cp -r /home/xubuntu/puppet/manifests/site.pp /etc/puppet/manifests/
-echo "***************************"
+echo "*******************************************"
 echo "Asennus valmis."
-echo "***************************"
+echo "*******************************************"
