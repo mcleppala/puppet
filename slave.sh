758367cc1903ab8237df6374275021f57a5a58f8
mcleppala <minna.leppala@outlook.com>
2017-11-13 06:34:15 +0000

    Update slave.sh

--- slave.sh
+++ slave.sh
@@ -1,11 +1,28 @@
+echo "*******************************************"
+echo "Asetetaan näppäimistön kieli ja aikavyöhyke"
+echo "*******************************************"
+setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
+echo "*******************************************"
+echo "Asennetaan Puppet ja Tree"
+echo "*******************************************"
 sudo apt-get update
 sudo apt-get install -y puppet tree
+echo "*******************************************"
+echo "Asetetaan hostname ja editoidaan hosts-tiedosto"
+echo "*******************************************"
 hostnamectl set-hostname slave
 grep master /etc/hosts || echo -e "\n192.168.1.103 master\n"|sudo tee -a /etc/hosts
 grep ^server /etc/puppet/puppet.conf || echo -e "\n[agent]\nserver=master\n" |sudo tee -a /etc/puppet/puppet.conf
+echo "*******************************************"
+echo "Tehdään Puppetin uudelleen käynistys, ssl-kansion siivous ja testiyhteys"
+echo "*******************************************"
 sudo service puppet restart
 sudo service puppet stop
 sudo rm -r /var/lib/puppet/ssl
 sudo service puppet start
 sudo puppet agent --enable
 sudo puppet agent -tdv
+echo "*******************************************"
+echo "Asennus valmis."
+echo "*******************************************"
