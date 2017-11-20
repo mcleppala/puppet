5df64b7ee15d32d51deb6f4e2c2efb6b6f8d7b65
mcleppala <minna.leppala@outlook.com>
2017-11-20 12:35:31 +0200

    Update ansible-master.sh

--- andible/ansible-master.sh
+++ andible/ansible-master.sh
@@ -11,7 +11,7 @@ sudo apt-get install -y ansible tree openssh-server openssh-client ssh
 echo "*******************************************"
 echo "Muokataan /etc/ansible/hosts-tiedostoa"
 echo "*******************************************"
-echo -e "\n[orja]\n192.168.1.102\n"|sudo tee -a /etc/hosts
+echo -e "\n[orja]\n192.168.1.102\n"|sudo tee -a /etc/ansible/hosts
 echo "*******************************************"
 echo "Luodaan ssh-avain ja kopioidaan se orjalle"
 echo "*******************************************"
