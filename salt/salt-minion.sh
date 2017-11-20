48953e2d722c95783dc00d3aa072ad6ac6ce7143
mcleppala <minna.leppala@outlook.com>
2017-11-20 13:58:10 +0200

    Create salt-minion.sh

---
+++ salt/salt-minion.sh
@@ -0,0 +1,16 @@
+echo "*******************************************"
+echo "Asetetaan näppäimistön kieli ja aikavyöhyke"
+echo "*******************************************"
+setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
+echo "*******************************************"
+echo "Asennetaan Salt-master ja Tree"
+echo "*******************************************"
+sudo apt-get update
+sudo apt-get install -y salt-minion tree
+echo "*******************************************"
+echo "Muokataan /etc/ansible/hosts-tiedostoa"
+echo -e "\nmaster: 192.168.1.102\n"|sudo tee -a /etc/salt/minion
+echo "*******************************************"
+echo "Asennus valmis"
+echo "*******************************************"
