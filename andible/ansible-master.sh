46ae1c420921fb14d3ad85cbb53a4c310348d963
mcleppala <minna.leppala@outlook.com>
2017-11-20 10:23:03 +0000

    Create ansible-master.sh

---
+++ andible/ansible-master.sh
@@ -0,0 +1,18 @@
+echo "*******************************************"
+echo "Asetetaan näppäimistön kieli ja aikavyöhyke"
+echo "*******************************************"
+setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
+echo "*******************************************"
+echo "Asennetaan Ansible, Tree ja OpenSSH"
+echo "*******************************************"
+sudo apt-get update
+sudo apt-get install -y ansible tree openssh-server openssh-client ssh
+echo "*******************************************"
+echo "Muokataan /etc/ansible/hosts-tiedostoa"
+echo "*******************************************"
+echo -e "\n[orja]\n192.168.1.102\n"|sudo tee -a /etc/hosts
+echo "*******************************************"
+echo "Luodaan ssh-avain ja kopioidaan se orjalle"
+echo "*******************************************"
+
