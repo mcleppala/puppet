3c62fa642dc3f37a31e389ce654f77683c98a812
mcleppala <minna.leppala@outlook.com>
2017-11-20 13:56:32 +0200

    Create salt-master.sh

---
+++ salt/salt-master.sh
@@ -0,0 +1,16 @@
+echo "*******************************************"
+echo "Asetetaan näppäimistön kieli ja aikavyöhyke"
+echo "*******************************************"
+setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
+echo "*******************************************"
+echo "Asennetaan Salt-master ja Tree"
+echo "*******************************************"
+sudo apt-get update
+sudo apt-get install -y salt-master tree
+echo "*******************************************"
+echo "Muokataan /etc/ansible/hosts-tiedostoa"
+echo -e "\ninterface: 192.168.1.102\n"|sudo tee -a /etc/salt/master
+echo "*******************************************"
+echo "Asennus valmis"
+echo "*******************************************"
